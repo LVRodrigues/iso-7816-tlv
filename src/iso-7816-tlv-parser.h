232b427f93be375a644ce88827dbeef176be32d1
LVRodrigues <lvrodriguesline@gmail.com>
2022-03-18 15:53:00 -0300

    Parser TLV.

--- src/iso-7816-tlv-parser.h
+++ src/iso-7816-tlv-parser.h
@@ -13,12 +13,37 @@
 
 #include "iso-7816-tlv.h"
 #include <vector>
+#include <queue>
 
 /**
  * @brief Analisador de elementos no protocolo TLV.
  */
 class TLVParser {
+        /**
+         * @brief Converte uma matriz de byte em um valor Inteiro de 32 bits.
+         * 
+         * @param[in] buffer Matriz de bytes.
+         * @return uint32_t Valor inteiro.
+         */
+        static uint32_t toInt32(std::vector<uint8_t> buffer);
 
+        /**
+         * @brief Extrai o campo tag de uma matriz de bytes.
+         * 
+         * Os bytes da tag são removidos da matriz de bytes.
+         * 
+         * @param[in,out] queue Matriz de bytes
+         * @return uint32_t Tag identificada.
+         */
+        static uint32_t parseTag(std::queue<uint8_t> &queue);
+
+        /**
+         * @brief Extrai o campo de tamanho dos dados na matriz de bytes.
+         * 
+         * @param[in,out] queue Matriz de bytes.
+         * @return uint32_t Tamanho do campo.
+         */
+        static uint32_t parseLength(std::queue<uint8_t> &queue);
     public:
         /**
          * @brief Analisa um fluxo de bytes no formato hexadecimal e extrai elementos
--- test/parser-test.cpp
+++ test/parser-test.cpp
@@ -1,2 +1,8 @@
 #include <gtest/gtest.h>
 #include <iso-7816-tlv-parser.h>
+
+TEST(PARSER, PARSE_1) {
+    std::string data = "050A0431190501123CB707DE";
+    std::vector<TLV> result = TLVParser::parse(data);
+    ASSERT_EQ(1, result.size());
+}
