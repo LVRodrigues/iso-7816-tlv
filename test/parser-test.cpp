9995facb3bd7d891686d9a76c75a122b94636af0
LVRodrigues <lvrodriguesline@gmail.com>
2022-03-21 06:47:12 -0300

    Teste de decomposição de múltipos registros TLV.

--- test/parser-test.cpp
+++ test/parser-test.cpp
@@ -1,8 +1,21 @@
 #include <gtest/gtest.h>
 #include <iso-7816-tlv-parser.h>
+#include <sstream>
 
 TEST(PARSER, PARSE_1) {
     std::string data = "050A0431190501123CB707DE";
     std::vector<TLV> result = TLVParser::parse(data);
     ASSERT_EQ(1, result.size());
+}
+
+TEST(PARSER, PARSE_2) {
+    std::stringstream stream;
+    stream << "5F8703AABBCC";
+    stream << "1A81910102030405060708090A0102030405060708090A0102030405060708090A0102030405060708090A0102030405060708090A0102030405060708090A0102030405060708090A0102030405060708090A0102030405060708090A0102030405060708090A0102030405060708090A0102030405060708090A0102030405060708090A0102030405060708090A0102030405";
+    stream << "B105FEFEFEFEFE";
+    std::vector<TLV> result = TLVParser::parse(stream.str());
+    ASSERT_EQ(3, result.size());
+    ASSERT_EQ(0x5f87, result[0].getTag());
+    ASSERT_EQ(0x1a, result[1].getTag());
+    ASSERT_EQ(0xb1, result[2].getTag());
 }
